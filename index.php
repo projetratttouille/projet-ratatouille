4bcfe7f9cd90ce761774d3c1b050982e86c2d925
Yoann <yoann.gloaguen@gmail.com>
2016-09-14 15:20:19 +0200

    1st commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include('header.php'); ?>
+				
+<section>
+<div id="presentation">
+	<div class="image_de_fond">
+		<img src="images/restaurant-690569_1280.jpg" alt="légumes" />
+
+		<div class="texte_presentation">
+			<p>La franchise ratatouille vous présente ses restaurants. Dans un esprits d’authenticité et de savoir faire traditionnelle, les différents restaurants vous proposent de savoureux plats réalisé à partir de produits locaux et de grandes qualités. Les restaurants ratatouille vous accueille du lundi au dimanche dans un cadre chaleureux et conviviale.</p>
+		</div>
+
+	</div>
+
+</div>
+<div class="restos">
+	<ul class="center-block">
+		<li><img src="images/restaurant_lachimere_gf.jpg" alt="restaurant de lyon" class="img-circle"/></li>
+		<li><img src="images/alatraboule4_gf.jpg" alt="restaurant Toulouse" class="img-circle"/></li>
+	</ul>
+</div>
+</section>
+<?php include('footer.php'); ?>
+
