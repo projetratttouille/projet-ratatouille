90f8c0099359b3c340bdac878f16287c10cd8584
tanguilb <tangui.lebourdonnec@gmail.com>
2016-09-14 15:57:43 +0200

    Ajout du texte dans le fichier nous.php

--- nous.php
+++ nous.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<title>Ratatouille</title>
-			<meta http-equiv="content-type" content="text/html" charset="utf-8" />
-			<link rel="stylesheet" href="style.css" />
-		</head>
-
-		<body>
-			<div id="bloc_page">
-				<?php include('header.php'); ?>
+<?php include('header.php'); ?>
 				
-				<section>
-				</section>
-				<?php include('footer.php'); ?>
-			</div>
-			
-		</body>
-	</html>
+<section>
+
+	<h1>Qui sommes nous?</h1>
+	<h4>Le groupe</h4>
+	<p>Nous sommes deux amis et collaborateur: Damien Armenté et Laurent Aubertin, passionné de cuisine fraçaise et de bon produits. Nous souhaiton proposer des plats régionnaux élaborés à partir d'ingrédients issus des producteurs locaux.</p>
+	<h4>L'histoire</h4>
+	<br />
+	<p>Créé en 2012 à La loupe un petit village du Perche, nous avons expérimenté notre concept auprès des habitants et des producteurs locaux. Au vu des retours positifs des clients de notre restaurant, nous avons décidé de nous mettre en franchise pour permettre à d’autre restaurateur d’ouvrir des restaurants basé sur ce concept de proximité et de convivialité. dès 2013 d’autres collaborateurs nous ont rejoint et ou ouvert leur enseigne à chartres orléans et fontainebleau.<br /> 
+Notre renommés grandissantes cette année nous avons permis à d’autre chef d’ouvrir des restaurants à Toulouse, Lyon, et Bordeaux.<br /> 
+Nous souhaitons toujours permettrent à d’autre cuisinier en phase avec notre concept et nos valeurs d’ouvrir leur propre lieu gastronomique dans leur ville respective.</p>
+	<br />
+	<h4>Chiffre Clés : </h4>
+	<br />
+	<p>En 2016: Nous avons 7 restaurants ouverts sur toutes la France.<br />
+	14 collaborateurs.<br />
+	1 millions d'euros de chiffre d'affaires en 2016.<br />
+	99% de clients satisfait.</p>
+
+</section>
+<?php include('footer.php'); ?>
