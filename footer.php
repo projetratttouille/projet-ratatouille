49b203a722ca906c439d3b2933742b017c44cd17
Yoann <yoann.gloaguen@gmail.com>
2016-09-14 17:05:05 +0200

    Modifications du footer

--- footer.php
+++ footer.php
@@ -1,16 +1,12 @@
 				<footer>
-					<nav>
-						<ul class="nav nav-pills">	
-							<li>
-								<a href="contact.php">Contact</a>
-							</li>
-							<li>
-								<a href="mention_legale.php">Mentions légales</a>
-							</li>
-						</ul>
-					</nav>
+					<div class="raw">
+						<p
+							class="col-xs-8">ratatouille.com © 2016 |
+							<a href="mention_legale.php">Mentions légales</a> |
+							<a href="contact.php">Contact</a>
+						</p>
+					</div>
 				</footer>
 			</div>
-			
 		</body>
 </html>
